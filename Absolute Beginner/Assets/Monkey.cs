f7939b7d91a2e29aee9ee3f5a119f8231c4f3b74
GuptaGod <43690128+GuptaGod@users.noreply.github.com>
2018-09-29 21:14:09 -0400

    First time unity team
    Lohith Gangula
    Jared Staman
    Diego Ferrer
    
    Co-Authored-By: jstaman <jstaman@users.noreply.github.com>
    Co-Authored-By: BonanPup <bonanpup@users.noreply.github.com>

---
+++ Absolute Beginner/Assets/Monkey.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Monkey : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    private const float Z = 0.125f;
+    public Transform player;
+    bool ded = false;
+    void Update()
+    {
+        //Look at the player
+        if (Input.GetKey("p"))
+        {
+            SceneManager.LoadScene("game");
+        }
+        if (!ded)
+        {
+            transform.LookAt(player);
+            transform.Translate(0.0f, 0.0f, Z);
+        }
+        else if (ded)
+        {
+            SceneManager.LoadScene("Game Over");
+        }
+        
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.name == "player")
+        {
+            Destroy(collision.gameObject);
+            ded = true;
+            
+        }
+    }
+}
---
+++ Absolute Beginner/Assets/prefab/PlayerControl.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    bool grounded = false;
+    bool gameOver = false;
+    bool win = false;
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        /*
+        var x = 3 * Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = 3 * Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+        */
+        float move = 0.15f;
+        float turn = 4.0f;
+        if(Input.GetKey("w"))
+        {
+            transform.Translate(0, 0, move);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(0, 0, -move);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(-move, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(move, 0, 0);
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.Rotate(0, -turn, 0);
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(0, turn, 0);
+        }
+
+        if (gameOver == true)
+        {
+            //scene change
+        }
+
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "GameGround")
+        {
+            grounded = true;
+        }
+        if (collision.gameObject.name == "monkey")
+        {
+            //scene change
+            gameOver = true;
+        }
+        if(collision.gameObject.name =="trophie_gold_A")
+        {
+            win = true;
+            //change scenes 
+        }
+    }
+}
